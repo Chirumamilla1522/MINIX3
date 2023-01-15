258672e9bc52f6a7e414d8e8fbf1fc26ddda94fe
chirumamilla bhargav <84395665+Chirumamilla1522@users.noreply.github.com>
2023-01-16 00:01:29 +0530

    Update half.c

--- Lab2/Part_2/half.c
+++ Lab2/Part_2/half.c
@@ -1,44 +1,35 @@
 
-#include <sys/wait.h>  //for waitpid()
-#include <unistd.h>    //for chdir(), fork(), exec(), pid_t
-#include <stdlib.h>    //for malloc(), realloc(), free(), exit(), execvp(), EXIT_FAILURE, EXIT_SUCESSS
-#include <stdio.h>     //for stderr, getchar(), perror(), printf(), scanf()
-#include <string.h>    //for strcmp(), strtok()
-#include <sys/types.h> //for pid, exec() stuff
-
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
 
-int main(int argc, char **argv)
-{
-    char **cmd = malloc(1024 * sizeof(char *));
-    int i;
+char* itoa(int val, int base){
+    
+    static char buf[32] = {0};    
+    int i = 30;    
+    for(; val && i ; --i, val /= base)    
+        buf[i] = "0123456789abcdef"[val % base];   
+    return &buf[i+1];
+    
+}
 
-    for (int i = 1; i < argc; i++)
-        cmd[i - 1] = argv[i];
-    cmd[i - 1] = NULL;
-
-    int a = atoi(cmd[argc - 2]);
-    a = a / 2;
-
-    sprintf(cmd[argc - 2], "%d", a);
-
-    if (cmd[1] == NULL)
-    {
-        printf("%s\n", cmd[argc - 2]);
+int main(int argc, char* argv[]) {
+    if (argc == 1)
         exit(0);
+        
+    int n = atoi(argv[argc - 1]);
+    int res = n / 2;
+    printf("Halving, Current process id: %u, Current result: %d\n", getpid(), res);
+
+    char* v[argc];
+    for (int i = 1; i < (argc-1) ; i++) {
+        v[i-1] = argv[i];
     }
-
-    char str[80];
-    strcpy(str, "./");
-    strcat(str, cmd[0]);
-    cmd[0] = str;
-
-    printf("PID of %s: %d\n", cmd[0], getpid());
-    if (execvp(cmd[0], cmd) < 0)
-    {
-        printf("cmd failed\n");
-        exit(0);
-    }
-
-    return 0;
+        
+    v[argc-2] = itoa(res, 10);
+    v[argc-1] = NULL;
+    
+    execvp(v[0], v);
 }
